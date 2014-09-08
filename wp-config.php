e4897e0fc45c53a0ef604e494582ac4672c1c6fe
Vũ Duy Từ <duytucntt@gmail.com>
2014-09-08 08:56:44 +0700

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -22,7 +22,7 @@ define('DB_NAME', 'professi');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', '127.0.0.1');
